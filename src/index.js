0cf7a7a55503a1303ed4b3378fa8e1873d14c06a
Raymond Zhou <razh@users.noreply.github.com>
2016-08-20 21:49:27 -0400

    Set-up GL context

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+/* global c */
+
+function render(el) {
+  var gl = el.getContext('webgl');
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+render(c);
