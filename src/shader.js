9bec3d0dfe71e4432d7f90d0deb885b13812b896
Raymond Zhou <razh@users.noreply.github.com>
2016-08-21 00:45:04 -0400

    Add shader indices function

--- src/shader.js
+++ src/shader.js
@@ -25,6 +25,13 @@ export function setFloat32Attribute(gl, program, name, size, array) {
   gl.vertexAttribPointer(location, size, gl.FLOAT, false, 0, 0);
 }
 
+export function createIndicesBuffer(gl, indices) {
+  var indicesBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+  return indicesBuffer;
+}
+
 export function setMat4Uniform(gl, program, name, array) {
   var location = gl.getUniformLocation(program, name);
   gl.uniformMatrix4fv(location, false, array);
