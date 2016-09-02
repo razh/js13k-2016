abb1582aed25ccecb5c43c0d2e661afd8676e93c
Raymond Zhou <razh@users.noreply.github.com>
2016-09-02 18:49:39 -0400

    Convert Material to MeshPhongMaterial

--- src/material.js
+++ src/material.js
@@ -1,1 +1,13 @@
-export function material_create() {}
+import { color_create } from './color';
+
+// MeshPhongMaterial.
+export function material_create() {
+  return {
+    color: color_create(1, 1, 1),
+    // 0x111111
+    specular: color_create(1 / 15, 1 / 15, 1 / 15),
+    shininess: 30,
+    emissive: color_create(),
+    emissiveIntensity: 1,
+  };
+}
