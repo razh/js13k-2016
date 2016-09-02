317bc288e8fbb3a7156a76aafc27195728c4c5d5
Raymond Zhou <razh@users.noreply.github.com>
2016-09-02 18:40:58 -0400

    Add Light default params

--- src/directionalLight.js
+++ src/directionalLight.js
@@ -1,8 +1,15 @@
 import { light_create } from './light';
+import { object3d_create } from './object3d';
 import { vec3_copy, vec3_Y } from './vec3';
 
 export function directionalLight_create(color, intensity) {
-  const light = light_create(color, intensity);
+  const light = Object.assign(
+    light_create(color, intensity),
+    {
+      target: object3d_create(),
+    }
+  );
+
   vec3_copy(light.position, vec3_Y);
   return light;
 }
--- src/light.js
+++ src/light.js
@@ -1,12 +1,12 @@
 import { color_create } from './color';
 import { object3d_create } from './object3d';
 
-export function light_create() {
+export function light_create(color, intensity) {
   return Object.assign(
     object3d_create(),
     {
-      color: color_create(),
-      intensity: 1,
+      color: color || color_create(),
+      intensity: intensity !== undefined ? intensity : 1,
     }
   );
 }
