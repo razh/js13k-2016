c86e9f6eaac6080f045d35c8f131410c43b70c0d
Raymond Zhou <razh@users.noreply.github.com>
2016-09-13 01:06:13 -0400

    Remove Light base class

--- src/directionalLight.js
+++ src/directionalLight.js
@@ -1,11 +1,12 @@
-import { light_create } from './light';
 import { object3d_create } from './object3d';
-import { vec3_Y } from './vec3';
+import { vec3_create, vec3_Y } from './vec3';
 
 export function directionalLight_create(color, intensity) {
   var light = Object.assign(
-    light_create(color, intensity),
+    object3d_create(),
     {
+      color: color || vec3_create(),
+      intensity: intensity !== undefined ? intensity : 1,
       target: object3d_create(),
     }
   );
