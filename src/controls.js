47fe5ab28fa0b22b4cf927daf10b53fae385533b
razh <razh@users.noreply.github.com>
2021-08-31 21:35:27 -0400

    Limit camera pitch angle

--- src/controls.js
+++ src/controls.js
@@ -1,9 +1,14 @@
-import { quat_create, quat_copy, quat_set, quat_normalize, quat_multiply } from './quat.js';
+import { clamp } from './math.js';
+import { quat_create, quat_multiply, quat_setFromEuler } from './quat.js';
+import { vec3_create } from './vec3.js';
 
 var pitchQuat = quat_create();
 var yawQuat = quat_create();
 
 export function controls_create(object) {
+  var pitchEuler = vec3_create();
+  var yawEuler = vec3_create();
+
   var controls = {
     object: object,
     speed: 1,
@@ -16,19 +21,16 @@ export function controls_create(object) {
         return;
       }
 
-      var movementX = event.movementX || event.mozMovementX || 0;
-      var movementY = event.movementY || event.mozMovementY || 0;
+      pitchEuler.x -= event.movementY * controls.sensitivity;
+      yawEuler.y -= event.movementX * controls.sensitivity;
 
-      var pitch = -movementY * controls.sensitivity;
-      var yaw = -movementX * controls.sensitivity;
+      pitchEuler.x = clamp(pitchEuler.x, -Math.PI / 2, Math.PI / 2);
 
-      quat_normalize(quat_set(pitchQuat, pitch, 0, 0, 1));
-      quat_normalize(quat_set(yawQuat, 0, yaw, 0, 1));
+      quat_setFromEuler(pitchQuat, pitchEuler);
+      quat_setFromEuler(yawQuat, yawEuler);
 
-      // pitch * object * yaw
-      quat_multiply(object.quaternion, pitchQuat);
-      quat_multiply(yawQuat, object.quaternion);
-      quat_copy(object.quaternion, yawQuat);
+      quat_multiply(yawQuat, pitchQuat);
+      Object.assign(object.quaternion, yawQuat);
     },
   };
 
